b0ea3f6e408ddde6da0e24eedc65da2753e5fbbf
jlmacle <jeanlouis.macle@gmail.com>
2021-03-02 14:19:12 +0100

    Code analysis and code modification.

--- src/test/java/jl/forthem/RecyclingPlacesBackendApplicationTests.java
+++ src/test/java/jl/forthem/RecyclingPlacesBackendApplicationTests.java
@@ -1,13 +1,15 @@
 package jl.forthem;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertTrue;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RecyclingPlacesBackendApplicationTests {
-
+	private boolean test=false;
 	@Test
 	void contextLoads() {
+		assertTrue(test);
 	}
 
 }
